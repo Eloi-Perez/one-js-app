a391f574bcd616eb04ef275f42fc13f8e80dbeb2
Eloi-Perez <eloi104@hotmail.com>
2021-03-11 16:13:18 +0000

    start

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza'
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']};
+pokemonList[0] = {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']};
+pokemonList[0] = {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']};

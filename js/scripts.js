5a5577165acb7db09bfaa48f0caff0c0b53966ea
Eloi-Perez <eloi104@hotmail.com>
2021-03-17 22:12:43 +0000

    new API

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,54 @@
-let pokemonRepository = (function () {
+let pokemonRepository = ( () => {
 
-    let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-    {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
-    {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, type: ['Fire']},
-    {name: 'Charmeleon', height: 1.1, type: ['Fire']},
-    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-    {name: 'Squirtle', height: 0.5, type: ['Water']},
-    {name: 'Wartortle', height: 1, type: ['Water']},
-    {name: 'Blastoise', height: 1.6, type: ['Water']}
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function getAll() {
         return pokemonList;
     }
 
     function add(pokemon) {
-        if (typeof(pokemon) === 'object' && 'name' in pokemon && 'height' in pokemon && 'type' in pokemon) {
+        if (typeof(pokemon) === 'object' && 'name' in pokemon) {
             pokemonList.push(pokemon);
         } else {
-            console.warn('pokemonRepository.add() can only add Objects that contains the properties: name, height and type.');
+            console.warn('%cpokemonRepository.add() can only add Objects that contains the properties: name' + '%c, height and type.', 'color:white;', 'color:grey; text-decoration: line-through;');
         }
+    }
+
+    function loadList() {
+        return fetch(apiUrl).then(response => {
+            return response.json();
+        }).then(json => {
+            json.results.forEach(item => { //results from JSON key
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(e => {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(response => {
+            return response.json();
+        }).then(details => {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(e => {
+            console.error(e);
+        });
     }
 
     function find(pokemon) {
         return pokemonList.filter(el => el['name'].toLowerCase() === pokemon.toLowerCase());
     }
 
-    function addListItem(pokemon){
+    function addListItem (pokemon) {
         let htmlList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
@@ -43,7 +63,9 @@ let pokemonRepository = (function () {
     }
 
     function showDetails(pokemon) {
-        console.log(pokemon.name +' '+ pokemon.height +'m '+ pokemon.type);
+        loadDetails(pokemon).then( () => {
+            console.log(pokemon.name +' '+ pokemon.height +'m ');   //+ pokemon.type ?
+       });
     }
 
     return {
@@ -51,22 +73,27 @@ let pokemonRepository = (function () {
         find: find,
         getAll: getAll,
         addListItem: addListItem,
-        showDetails: showDetails
+        showDetails: showDetails,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 
 })();
 
-let testAdd = {name: 'Pikachu', height: 0.4, type: ['Electric']};
-pokemonRepository.add(testAdd);
-
-pokemonRepository.getAll().map(element => {
-    pokemonRepository.addListItem(element);
+pokemonRepository.loadList().then( () => {
+    pokemonRepository.getAll().map(element => {
+        pokemonRepository.addListItem(element);
 
 
-    // let big = ''
-    // if (element.height > 1.1) {big = ' Wow, that’s big!'} else { big = ''}
-    // document.write(`<p><span class="name">${element.name}</span> <span class="height">height: ${element.height}m</span> <span class="type">${element.type}</span><span class="big"> ${big}</span></p>`);
+        // let big = ''
+        // if (element.height > 1.1) {big = ' Wow, that’s big!'} else { big = ''}
+        // document.write(`<p><span class="name">${element.name}</span> <span class="height">height: ${element.height}m</span> <span class="type">${element.type}</span><span class="big"> ${big}</span></p>`);
+    });
 });
 
+console.log(pokemonRepository.find('charizarD')); //now is happening before loading list
 
-console.log(pokemonRepository.find('charizarD'));
+
+
+let testAdd = {namse: 'Pikachu', height: 0.4, type: ['Electric']};
+pokemonRepository.add(testAdd);
